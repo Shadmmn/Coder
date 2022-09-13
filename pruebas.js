24e2b43cb865b140e0f36a3559932427f9d4a48e
Shadmn <francisco.cuello@outlook.com>
2022-09-13 19:12:30 -0300

    init

---
+++ pruebas.js
@@ -0,0 +1,25 @@
+
+let edad = 23;
+const ALTURA = 165;
+ console.log("lA ALTURA ES", ALTURA);
+
+let numero = 23;
+let numeroDos= 22;
+let suma = numero + numeroDos;
+
+let saludo = "hola ";
+let nombre = "franco"
+
+
+let mensaje = saludo + " " + nombre;
+let mensajeUno= `${saludo} ${nombre} + Que se cuenta?`;
+console.log(mensajeUno)
+console.log(mensaje)
+// esos simbolos ` sirven para reemplazar variables 
+
+let nombre2 = prompt("ingresa tu nombre"); 
+after = alert (saludo+nombre2);
+
+if(nombre===true){
+    alert(Bienvenido)
+}
---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+function mostrar(animal = prompt("que animal eres")) {
+if(animal === "perro"){
+    alert("woof")
+}
+else if(animal === "gato"){
+    alert("meow")
+}else if(animal === "caballo"){
+    alert("hiii")
+}else if(animal === "pato"){
+    alert("cuack")
+}else if(animal === "gallo"){
+    alert("cococorococoooo")
+}else if(animal === "leon"){
+    alert("rooooooar")
+}
+else{
+    alert("no conozco ese bicho")
+}
+}
